08678b6eed40a0a6fa9502d4245059f0b2652a5e
YouSS <youss.cs@gmail.com>
2019-04-13 18:05:08 +0100

    Size Class Landscape Layout

--- TinderClone/Controllers/RegistrationController.swift
+++ TinderClone/Controllers/RegistrationController.swift
@@ -17,6 +17,7 @@ class RegistrationController: UIViewController {
         button.backgroundColor = .white
         button.setTitleColor(.black, for: .normal)
         button.heightAnchor.constraint(equalToConstant: 275).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 275).isActive = true
         button.layer.cornerRadius = 16
         return button
     }()
@@ -53,20 +54,23 @@ class RegistrationController: UIViewController {
         return button
     }()
     
-    lazy var stackView = UIStackView(arrangedSubviews: [
-        selectPhotoButton,
-        fullNameTextField,
-        emailTextField,
-        passwordTextField,
-        registerButton
-        ])
+    let gradientLayer = CAGradientLayer()
+    
+    lazy var verticalStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [fullNameTextField, emailTextField, passwordTextField, registerButton])
+        stack.axis = .vertical
+        stack.spacing = 8
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    lazy var overallStackView = UIStackView(arrangedSubviews: [selectPhotoButton, verticalStackView])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupGradientLayer()
         setupLayout()
-        
         setupKeyboardObservers()
         setupTapGesture()
     }
@@ -74,6 +78,16 @@ class RegistrationController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        gradientLayer.frame = view.bounds
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        overallStackView.axis = traitCollection.verticalSizeClass == .compact ? .horizontal : .vertical
     }
     
     fileprivate func setupKeyboardObservers() {
@@ -88,7 +102,7 @@ class RegistrationController: UIViewController {
     @objc func handleKeyboardShow(notification: Notification) {
         guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         let keyboardFrame = value.cgRectValue
-        let bottomSpace = view.frame.height - stackView.frame.height - stackView.frame.origin.y
+        let bottomSpace = view.frame.height - overallStackView.frame.height - overallStackView.frame.origin.y
         let difference = keyboardFrame.height - bottomSpace
         
         view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
@@ -105,15 +119,14 @@ class RegistrationController: UIViewController {
     }
     
     fileprivate func setupLayout() {
-        view.addSubview(stackView)
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.addSubview(overallStackView)
+        overallStackView.axis = .vertical
+        overallStackView.spacing = 8
+        overallStackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        overallStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     fileprivate func setupGradientLayer() {
-        let gradientLayer = CAGradientLayer()
         let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
         let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
         
